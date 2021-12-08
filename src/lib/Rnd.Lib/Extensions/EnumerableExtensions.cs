8d6fa01fc6611f5267460914384ec28ffcd9bc13
Jonas Kamsker <git.jonas@kamsker.at>
2021-12-08 18:49:44 +0100

    Added enumerable ext

---
+++ src/lib/Rnd.Lib/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Rnd.Lib.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> OrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable ?? Enumerable.Empty<T>();
+        }
+    }
+}
