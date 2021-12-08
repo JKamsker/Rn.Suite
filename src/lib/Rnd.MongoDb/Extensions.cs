4a629695b6c852625ae18a88f2ab3fb49ebc7eb0
Jonas Kamsker <git.jonas@kamsker.at>
2021-12-08 18:48:56 +0100

    Added Mongo ext

--- src/lib/Rnd.MongoDb/Extensions.cs
+++ src/lib/Rnd.MongoDb/Extensions.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
 
@@ -25,6 +30,27 @@ namespace Rnd.MongoDb
         public static FilterDefinition<T> Where<T>(this FilterDefinition<T> input, Expression<Func<T, bool>> expression)
         {
             return Builders<T>.Filter.And(input, Builders<T>.Filter.Where(expression));
+        }
+
+        public static async IAsyncEnumerable<T> EnumerateAsync<T>(this Task<IAsyncCursor<T>> cursorTask, [EnumeratorCancellation] CancellationToken token = default)
+        {
+            var cursor = await cursorTask;
+            await foreach (var item in cursor.EnumerateAsync(token))
+            {
+                yield return item;
+            }
+        }
+
+        public static async IAsyncEnumerable<T> EnumerateAsync<T>(this IAsyncCursor<T> cursor, [EnumeratorCancellation] CancellationToken token = default)
+        {
+            while (await cursor.MoveNextAsync())
+            {
+                foreach (var item in cursor.Current)
+                {
+                    yield return item;
+                }
+                token.ThrowIfCancellationRequested();
+            }
         }
     }
 }
