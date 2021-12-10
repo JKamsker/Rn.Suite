f473f36f652bd8853e6a2ec862b797a4c949b26e
Weirdo <git.jonas@kamsker.at>
2021-12-10 08:12:08 +0100

    Added SpanExtensions

---
+++ src/lib/Rnd.Lib/Extensions/SpanExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rnd.Lib.Extensions
+{
+    public static class SpanExtensions
+    {
+        public static bool Contains(this string[] array, string value, StringComparison comparison = StringComparison.Ordinal)
+            => array.AsSpan().Contains(value, comparison);
+
+        public static bool Contains(this Span<string> array, string value, StringComparison comparison = StringComparison.Ordinal)
+            => ((ReadOnlySpan<string>)array).Contains(value, comparison);
+
+        public static bool Contains(this ReadOnlySpan<string> array, string value, StringComparison comparison = StringComparison.Ordinal)
+        {
+            foreach (var item in array)
+            {
+                if (string.Equals(value, item, comparison))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ src/lib/Rnd.Lib/Utils/UrlExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Rnd.Lib.Extensions;
+
+namespace Rnd.Lib.Utils
+{
+    public class UriEx
+    {
+        public static bool IsValidUrl(string url)
+            => IsValid(url, new string[] { Uri.UriSchemeHttp, Uri.UriSchemeHttps });
+
+        public static bool IsValid(string url, params string[] schemes)
+        {
+            if (Uri.TryCreate(url, UriKind.Absolute, out Uri uriResult))
+            {
+                if (schemes.Length == 0)
+                {
+                    return true;
+                }
+
+                return schemes.Contains(uriResult.Scheme, StringComparison.OrdinalIgnoreCase);
+            }
+            return false;
+
+            //bool passed = Uri.TryCreate(url, UriKind.Absolute, out Uri uriResult)
+            //&& (uriResult.Scheme == Uri.UriSchemeHttp
+            //  || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+    }
+}
