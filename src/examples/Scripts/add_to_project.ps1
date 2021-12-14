f300bb11462be1c06ebb46b0e67d012b7c03f555
Jonas Kamsker <git.jonas@kamsker.at>
2021-12-14 23:16:08 +0100

    added init script

--- src/examples/Scripts/add_to_project.ps1
+++ src/examples/Scripts/add_to_project.ps1
@@ -4,3 +4,10 @@ dotnet sln add -s "external\Rn.Suite" .\external\Rn.Suite\src\lib\Rnd.Lib\Rnd.Li
 dotnet sln add -s "external\Rn.Suite" .\external\Rn.Suite\src\lib\Rnd.IO\Rnd.IO.csproj
 dotnet sln add -s "external\Rn.Suite" .\external\Rn.Suite\src\lib\Rnd.Logging\Rnd.Logging.csproj
 dotnet sln add -s "external\Rn.Suite" .\external\Rn.Suite\src\lib\Rnd.MongoDb\Rnd.MongoDb.csproj
+
+# git submodule add --force https://github.com/JKamsker/Rn.Suite.git "./external/Rn.Suite"
+
+# dotnet sln add -s "external/Rn.Suite" ./external/Rn.Suite/src/lib/Rnd.Lib/Rnd.Lib.csproj
+# dotnet sln add -s "external/Rn.Suite" ./external/Rn.Suite/src/lib/Rnd.IO/Rnd.IO.csproj
+# dotnet sln add -s "external/Rn.Suite" ./external/Rn.Suite/src/lib/Rnd.Logging/Rnd.Logging.csproj
+# dotnet sln add -s "external/Rn.Suite" ./external/Rn.Suite/src/lib/Rnd.MongoDb/Rnd.MongoDb.csproj
