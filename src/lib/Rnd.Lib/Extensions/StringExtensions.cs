c792c8efda5f6e74ed504d77609c91385aa66ad6
Weirdo <git.jonas@kamsker.at>
2021-12-19 14:19:11 +0100

    added stringEx

--- src/lib/Rnd.Lib/Extensions/StringExtensions.cs
+++ src/lib/Rnd.Lib/Extensions/StringExtensions.cs
@@ -37,5 +37,50 @@ namespace Rnd.Lib.Extensions
         {
             return endings.Any(x => input.EndsWith(x, stringComparison));
         }
+
+        public static string TrimStart
+        (
+            this string target,
+            string trimString,
+            StringComparison stringComparison = StringComparison.OrdinalIgnoreCase
+        )
+        {
+            if (string.IsNullOrEmpty(trimString)) return target;
+
+            string result = target;
+            while (result.StartsWith(trimString, stringComparison))
+            {
+                result = result.Substring(trimString.Length);
+            }
+
+            return result;
+        }
+
+        public static string TrimEnd(this string inputText, string value, StringComparison comparisonType = StringComparison.CurrentCultureIgnoreCase)
+        {
+            if (!string.IsNullOrEmpty(value))
+            {
+                while (!string.IsNullOrEmpty(inputText) && inputText.EndsWith(value, comparisonType))
+                {
+                    inputText = inputText.Substring(0, (inputText.Length - value.Length));
+                }
+            }
+
+            return inputText;
+        }
+
+
+        public static string ConcatString(this IEnumerable<string> enumerable) => string.Concat(enumerable);
+
+        public static string ConcatString<T>(this IEnumerable<T> enumerable, Func<T, string> selector) => string.Concat(enumerable.Select(selector));
+
+        public static string JoinString(this IEnumerable<string> enumerable, char seperator) => string.Join(seperator, enumerable);
+
+        public static string JoinString(this IEnumerable<string> enumerable, string seperator) => string.Join(seperator, enumerable);
+
+        public static string JoinString<T>(this IEnumerable<T> enumerable, string seperator, Func<T, string> selector) => string.Join(seperator, enumerable.Select(x => selector(x)));
+
+        public static string JoinString<T>(this IEnumerable<T> enumerable, char seperator, Func<T, string> selector) => string.Join(seperator, enumerable.Select(x => selector(x)));
+
     }
 }
